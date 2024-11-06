1d26a6e5360db08b848cd89b87683eb42773221b
iampassos <78429887+iampassos@users.noreply.github.com>
2024-11-06 16:15:07 -0300

    feat: add real objectives

--- src/objectives.c
+++ src/objectives.c
@@ -4,10 +4,27 @@
 struct Objective {
   int id;
   char *description;
-} objectives[3] = {
-    {.id = 0, .description = "Test0"},
-    {.id = 1, .description = "Test1"},
-    {.id = 2, .description = "Test2"},
-};
+} objectives[] = {
+    {.id = 0, .description = "Conquer 24 territories"},
+    {.id = 1, .description = "Eliminate the yellow army"},
+    {.id = 2, .description = "Eliminate the blue army"},
+    {.id = 3, .description = "Eliminate the green army"},
+    {.id = 4, .description = "Eliminate the black army"},
+    {.id = 5, .description = "Eliminate the red army"},
+    {.id = 6, .description = "Eliminate the white army"},
+    {.id = 7, .description = "Conquer North America and Africa"},
+    {.id = 8, .description = "Conquer North America and Oceania"},
+    {.id = 9, .description = "Conquer Europe and South America"},
+    {.id = 10, .description = "Conquer Europe and Oceania"},
+    {.id = 11, .description = "Conquer Asia and South America"},
+    {.id = 12, .description = "Conquer Asia and Africa"},
+    {.id = 13,
+     .description = "Conquer 18 territories with at least two armies in each"},
+    {.id = 14, .description = "Conquer 30 territories"},
+    {.id = 15, .description = "Conquer South America and Africa"},
+    {.id = 16, .description = "Conquer North America and Asia"},
+    {.id = 17, .description = "Conquer Europe and Asia"},
+    {.id = 18, .description = "Conquer Asia and Oceania"},
+    {.id = 19, .description = "Conquer North America and Europe"}};
 
 #endif
